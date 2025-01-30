4200bc6443b516acffcf2c8c461d2a29f0a9a3b9
Matheus Guilherme <mg.ls1995@gmail.com>
2025-01-29 21:34:18 -0300

    terceiro commit

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,9 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = []
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    
+    if (nome=='') {
+        alert ('Por favor, insira um nome.');
+    }
+}
