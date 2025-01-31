7c18403ac8f35913ef38ef57a2690946619cbd9e
Matheus Guilherme <mg.ls1995@gmail.com>
2025-01-30 21:07:52 -0300

    Implementação de função para atualizar a lista

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -11,3 +11,18 @@ function adicionarAmigo() {
 }
 return nome;
 }
+
+function atualizarLista() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li');
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+function limparCampo() {
+    document.querySelector('#amigo').valor = '';
+}
